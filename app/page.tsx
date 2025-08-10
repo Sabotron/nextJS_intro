0058210a58f784429add699202309c6406e8c758
sabotron <sabotronics@gmail.com>
2025-08-09 20:57:06 -0600

    Chapter 7 completed

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import {lusitana} from '@/app/ui/fonts';
 //importing Image component
 import Image from 'next/image'; 
 
-export default function Page() {
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
